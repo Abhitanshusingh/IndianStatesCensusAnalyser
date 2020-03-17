eff361df22d5c310fd3880628afd4d98b0e852ad
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-17 12:18:56 +0530

    ADDED: POJO Class to read csv file

---
+++ src/main/java/com/bridgelabz/model/CSVStateCensus.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.model;
+
+import com.opencsv.bean.CsvBindByName;
+//COLUMN NAME OF StateCensusAnalyserData
+public class CSVStateCensus {
+    @CsvBindByName
+    private String State;
+
+    @CsvBindByName
+    private String Population;
+
+    @CsvBindByName
+    private String AreaInSqKm;
+
+    @CsvBindByName
+    private String DensityPerSqkm;
+
+    //GETTER SETTER METHOD
+    public void setState(String state) {
+        State = state;
+    }
+
+    public void setPopulation(String population) {
+        Population = population;
+    }
+
+    public void setAreaInSqKm(String areaInSqKm) {
+        AreaInSqKm = areaInSqKm;
+    }
+
+    public void setDensityPerSqkm(String densityPerSqkm) {
+        DensityPerSqkm = densityPerSqkm;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public String getPopulation() {
+        return Population;
+    }
+
+    public String getAreaInSqKm() {
+        return AreaInSqKm;
+    }
+
+    public String getDensityPerSqkm() {
+        return DensityPerSqkm;
+    }
+}
