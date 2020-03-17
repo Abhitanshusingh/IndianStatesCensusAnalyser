963ab547a6bac2fd6bce54170e73032b29af125e
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-17 16:27:02 +0530

    ADDED: Method to handle type exception

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 
 public class StateCensusAnalyser {
     int countRecord = 0;
+
     //READING AND PRINTING DATA FROM CSV FILE
     public int loadCensusCsvData(String SAMPLE_CSV_PATH) throws StateCensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_PATH));) {
@@ -28,9 +29,31 @@ public class StateCensusAnalyser {
                 System.out.println("<=========================>");
                 countRecord++;
             }
-        }
-         catch (IOException e) {
+        } catch (IOException e) {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.Exceptiontype.FILE_NOT_FOUND, e.getMessage());
+        }
+        return countRecord;
+    }
+
+    //READING AND PRINTING DATA FROM CSV FILE AND HANDLED EXCEPTION INCORRECT TYPE
+    public int loadCensusCsvDataIncorrectType(String SAMPLE_CSV_PATH) throws StateCensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_PATH));) {
+            CsvToBean<CSVStateCensus> CsvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVStateCensus.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVStateCensus> csvUserIterator = CsvToBean.iterator();
+            while (csvUserIterator.hasNext()) {
+                CSVStateCensus csvUser = csvUserIterator.next();
+                System.out.println("State : " + csvUser.getState());
+                System.out.println("Population : " + csvUser.getPopulation());
+                System.out.println("AreaInSqKm : " + csvUser.getAreaInSqKm());
+                System.out.println("DensityPerSqkm : " + csvUser.getDensityPerSqkm());
+                System.out.println("<=========================>");
+                countRecord++;
+            }
+        } catch (IOException e) {
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE, e.getMessage());
         }
         return countRecord;
     }
