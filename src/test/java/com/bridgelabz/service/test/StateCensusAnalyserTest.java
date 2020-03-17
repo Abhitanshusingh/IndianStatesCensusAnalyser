e9b1faacddbb1a471489304c338de7b13804e0c8
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-17 16:25:21 +0530

    ADDED: Type is incorrect then exception is raised

--- src/test/java/com/bridgelabz/service/test/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/service/test/StateCensusAnalyserTest.java
@@ -9,9 +9,11 @@ public class StateCensusAnalyserTest {
     StateCensusAnalyser cencusAnalyser = new StateCensusAnalyser();
     private final String SIMPLE_CSV_PATH = "/home/bridgelabz/Desktop/JavaPrograms/IndianStateCensusAnalyser/src/test/resources/StateCensusData.csv";
     private final String INCORRECT_CSV_PATH = "/home/bridgelabz/Desktop/JavaProgram";
+    private final String INCORRECT_CSV_TYPE = "/home/bridgelabz/Desktop/JavaPrograms/IndianStateCensusAnalyser/src/test/resources/StateCensusData.pdf";
 
     @Test
-    public void givenStateCensusCsvFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException {
+    public void givenStateCensusCsvFile_WhenTrue_NumberOfRecordShouldMatch()
+            throws StateCensusAnalyserException {
         int totalRecords = cencusAnalyser.loadCensusCsvData(SIMPLE_CSV_PATH);
         Assert.assertEquals(29, totalRecords);
     }
@@ -21,7 +23,16 @@ public class StateCensusAnalyserTest {
         try {
             cencusAnalyser.loadCensusCsvData(INCORRECT_CSV_PATH);
         } catch (StateCensusAnalyserException e) {
-            Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.FILE_NOT_FOUND, e.type);
+            Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE, e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCensusCsvFile_WhenTypeIncorrect_ShouldThrowCustomException() {
+        try {
+            cencusAnalyser.loadCensusCsvData(INCORRECT_CSV_TYPE);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE, e.type);
         }
     }
 }
