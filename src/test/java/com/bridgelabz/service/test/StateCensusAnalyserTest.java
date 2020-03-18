64ac978854feb77e7149606a267eab287d19279d
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-03-18 12:58:08 +0530

    REFACTORED: Change directory and modified indentation

--- src/test/java/com/bridgelabz/service/test/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/service/test/StateCensusAnalyserTest.java
@@ -9,42 +9,53 @@ import java.io.File;
 
 public class StateCensusAnalyserTest {
     StateCensusAnalyser cencusAnalyser = new StateCensusAnalyser();
-    private final String SIMPLE_CSV_PATH = "/home/bridgelabz/Desktop/JavaPrograms/IndianStateCensusAnalyser/src/test/resources/StateCensusData.csv";
+    private final String SIMPLE_CSV_PATH = "./src/test/resources/StateCensusData.csv";
     private final String INCORRECT_CSV_PATH = "/home/bridgelabz/Desktop/JavaProgram";
-    private final String INCORRECT_CSV_TYPE = "/home/bridgelabz/Desktop/JavaPrograms/IndianStateCensusAnalyser/src/test/resources/StateCensusData.pdf";
-    private final String INCORRECT_DELIMITER = "/home/bridgelabz/Desktop/JavaPrograms/IndianStateCensusAnalyser/src/test/resources/StateCensusData1.csv";
+    private final String INCORRECT_CSV_TYPE = "./src/test/resources/StateCensusData.pdf";
+    private final String INCORRECT_DELIMITER = "./src/test/resources/StateCensusData1.csv";
 
     @Test
-    public void givenStateCensusCsvFile_WhenTrue_NumberOfRecordShouldMatch()
-            throws StateCensusAnalyserException {
+    public void givenStateCensusCsvFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException
+    {
         int totalRecords = cencusAnalyser.loadCensusCsvData(SIMPLE_CSV_PATH);
         Assert.assertEquals(29, totalRecords);
     }
 
     @Test
-    public void givenStateCensusCsvFile_WhenFileNameIncorrect_ShouldThrowCustomException() {
-        try {
+    public void givenStateCensusCsvFile_WhenFileNameIncorrect_ShouldThrowCustomException()
+    {
+        try
+        {
             cencusAnalyser.loadCensusCsvData(INCORRECT_CSV_PATH);
-        } catch (StateCensusAnalyserException e) {
+        }
+        catch (StateCensusAnalyserException e)
+        {
             Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.FILE_NOT_FOUND, e.type);
         }
     }
 
     @Test
-    public void givenStateCensusCsvFile_WhenTypeIncorrect_ShouldThrowCustomException() {
-        try {
-            File fileExtension = new File(INCORRECT_CSV_TYPE);
-            cencusAnalyser.getFileExtension(fileExtension);
-        } catch (StateCensusAnalyserException e) {
-            Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE, "FILE TYPE INCORRECT");
+    public void givenStateCensusCsvFile_WhenTypeIncorrect_ShouldThrowCustomException()
+    {
+        try
+        {
+            cencusAnalyser.getFileExtension(new File(INCORRECT_CSV_TYPE));
+        }
+        catch (StateCensusAnalyserException e)
+        {
+            Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE,e.type);
         }
     }
 
     @Test
-    public void givenStateCensusCsvFile_WhenDelimiterIncorrect_ShouldThrowCustomException() {
-        try {
+    public void givenStateCensusCsvFile_WhenDelimiterIncorrect_ShouldThrowCustomException()
+    {
+        try
+        {
             cencusAnalyser.loadCensusCsvData(INCORRECT_DELIMITER);
-        } catch (StateCensusAnalyserException e) {
+        }
+        catch (StateCensusAnalyserException e)
+        {
             Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.DELIMITER_INCORRECT, e.type);
         }
     }
