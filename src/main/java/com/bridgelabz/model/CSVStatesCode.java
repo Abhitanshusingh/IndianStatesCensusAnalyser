5d082e870795ae0b14ebec265750fb3ff19d6033
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-03-18 22:45:53 +0530

    ADDED: POJO class for CSVStateCode file

---
+++ src/main/java/com/bridgelabz/model/CSVStatesCode.java
@@ -0,0 +1,61 @@
+package com.bridgelabz.model;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStatesCode
+{
+    //COLUMN NAME OF StateCensusCode
+    @CsvBindByName
+    private String SrNo;
+
+    @CsvBindByName
+    private String StateName;
+
+    @CsvBindByName
+    private String TIN;
+
+    @CsvBindByName
+    private String StateCode;
+
+    //GETTER SETTER METHOD
+    public void setSrNo(String srNo)
+    {
+        SrNo = srNo;
+    }
+
+    public void setStateName(String stateName)
+    {
+        StateName = stateName;
+    }
+
+    public void setTIN(String TIN)
+    {
+        this.TIN = TIN;
+    }
+
+    public void setStateCode(String stateCode)
+    {
+        StateCode = stateCode;
+    }
+
+    public String getSrNo()
+    {
+        return SrNo;
+    }
+
+    public String getStateName()
+    {
+        return StateName;
+    }
+
+
+    public String getTIN()
+    {
+        return TIN;
+    }
+
+    public String getStateCode()
+    {
+        return StateCode;
+    }
+}
