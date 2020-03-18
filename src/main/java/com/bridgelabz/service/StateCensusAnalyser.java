e007d60092fc15f632d7545e7d23606b9457ccf5
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-03-18 12:48:54 +0530

    REFACTORED: Change in getFileExtension method and indentation

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -10,18 +10,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
-public class StateCensusAnalyser {
+public class StateCensusAnalyser
+{
     int countRecord = 0;
 
     //READING AND PRINTING DATA FROM CSV FILE
-    public int loadCensusCsvData(String SAMPLE_CSV_PATH) throws StateCensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_PATH));) {
+    public int loadCensusCsvData(String SAMPLE_CSV_PATH) throws StateCensusAnalyserException
+    {
+        try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_PATH));)
+        {
             CsvToBean<CSVStateCensus> CsvToBean = new CsvToBeanBuilder(reader)
                     .withType(CSVStateCensus.class)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
             Iterator<CSVStateCensus> csvUserIterator = CsvToBean.iterator();
-            while (csvUserIterator.hasNext()) {
+            while (csvUserIterator.hasNext())
+            {
                 CSVStateCensus csvUser = csvUserIterator.next();
                 System.out.println("State : " + csvUser.getState());
                 System.out.println("Population : " + csvUser.getPopulation());
@@ -30,22 +34,29 @@ public class StateCensusAnalyser {
                 System.out.println("<=========================>");
                 countRecord++;
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.Exceptiontype.FILE_NOT_FOUND, e.getMessage());
-        } catch (RuntimeException e) {
+        }
+        catch (RuntimeException e)
+        {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.Exceptiontype.DELIMITER_INCORRECT, e.getMessage());
         }
         return countRecord;
     }
 
     //READ FILE EXTENSION
-    public void getFileExtension(File file) throws StateCensusAnalyserException {
+    public void getFileExtension(File file) throws StateCensusAnalyserException
+    {
         String extension = "";
-        if (file != null && file.exists()) {
+        if (file != null)
+        {
             String name = file.getName();
             extension = name.substring(name.lastIndexOf("."));
-            if (extension != ".csv") {
-                throw new StateCensusAnalyserException(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE, "FILE TYPE INCORRECT");
+            if (!extension.equals(".csv"))
+            {
+                throw new StateCensusAnalyserException(StateCensusAnalyserException.Exceptiontype.ENTER_WRONG_TYPE, "FILE_TYPE_INCORRECT");
             }
         }
     }
