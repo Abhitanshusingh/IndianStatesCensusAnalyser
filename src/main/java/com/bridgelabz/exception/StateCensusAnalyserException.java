04ac2af8e05b3e0b5dd89cc1b9eb0d5ce354a77d
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-03-19 14:25:21 +0530

    ADDED: Exception type

--- src/main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
@@ -4,7 +4,7 @@ public class StateCensusAnalyserException extends Exception
 {
     public enum Exceptiontype
     {
-        FILE_NOT_FOUND,ENTER_WRONG_TYPE,INCORRECT_DELIMITER_OR_HEADER;
+        FILE_NOT_FOUND,ENTER_WRONG_TYPE,INCORRECT_DELIMITER_OR_HEADER,ILLEGAL_STATE;
     }
    public Exceptiontype type;
 
