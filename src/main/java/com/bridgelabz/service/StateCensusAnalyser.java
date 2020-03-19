464ac034a84d3bb71b8e631c802dfd02f36d855d
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-03-19 12:24:41 +0530

    ADDED: loadStateCodeCsvData method to read data from CSVStateCode file

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.service;
+
 import com.bridgelabz.exception.StateCensusAnalyserException;
 import com.bridgelabz.model.CSVStateCensus;
+import com.bridgelabz.model.CSVStatesCode;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
@@ -25,12 +28,7 @@ public class StateCensusAnalyser{
             Iterator<CSVStateCensus> csvUserIterator = CsvToBean.iterator();
             while (csvUserIterator.hasNext())
             {
-                CSVStateCensus csvUser = csvUserIterator.next();
-                System.out.println("State : " + csvUser.getState());
-                System.out.println("Population : " + csvUser.getPopulation());
-                System.out.println("AreaInSqKm : " + csvUser.getAreaInSqKm());
-                System.out.println("DensityPerSqkm : " + csvUser.getDensityPerSqkm());
-                System.out.println("<=========================>");
+                CSVStateCensus csvStateCensus = csvUserIterator.next();
                 countRecord++;
             }
         }
@@ -42,11 +40,39 @@ public class StateCensusAnalyser{
         catch (RuntimeException e)
         {
             throw new StateCensusAnalyserException
-                    (StateCensusAnalyserException.Exceptiontype.NO_SUCH_HEADER, e.getMessage());
+                    (StateCensusAnalyserException.Exceptiontype.INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
         }
         return countRecord;
     }
 
+    //READING AND PRINTING DATA FROM CSV FILE
+    public int loadSateCodeCsvData(String CSV_PATH) throws StateCensusAnalyserException
+    {
+        try (Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));)
+        {
+            CsvToBean<CSVStatesCode> CsvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVStatesCode.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVStatesCode> csvUserIterator = CsvToBean.iterator();
+            while (csvUserIterator.hasNext())
+            {
+                countRecord++;
+                CSVStatesCode csvStatesCode= csvUserIterator.next();
+            }
+            return countRecord;
+        }
+        catch (IOException e)
+        {
+            throw new StateCensusAnalyserException
+                    (StateCensusAnalyserException.Exceptiontype.FILE_NOT_FOUND, e.getMessage());
+        }
+        catch (RuntimeException e)
+        {
+            throw new StateCensusAnalyserException
+                    (StateCensusAnalyserException.Exceptiontype.INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
+        }
+    }
     //READ FILE EXTENSION
     public void getFileExtension(File file) throws StateCensusAnalyserException
     {
