5a0113fffc326205ddc0667f8d12e15e39df29c9
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-03-24 22:26:18 +0530

    REFACTORED: Change getCSVFileList into method generic

--- src/main/java/com/bridgelabz/service/OpenCSVBuilderImp.java
+++ src/main/java/com/bridgelabz/service/OpenCSVBuilderImp.java
@@ -8,44 +8,34 @@ import java.io.Reader;
 import java.util.Iterator;
 import java.util.List;
 
-public class OpenCSVBuilderImp<E> implements ICSVBuilder
-{
+public class OpenCSVBuilderImp<E> implements ICSVBuilder {
     //CREATE GENERIC METHOD TO ITERATE CSV FILE
     @Override
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException
-    {
-        try
-        {
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+        try {
             CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<E> csvToBean = csvToBeanBuilder.build();
             Iterator<E> censusCSVIterator = csvToBean.iterator();
             return censusCSVIterator;
-        }
-        catch (IllegalStateException e)
-        {
+        } catch (IllegalStateException e) {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.ILLEGAL_STATE, e.getMessage());
         }
     }
 
     @Override
-    public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException
-    {
-        return this.getCSVBean(reader, csvClass).parse();
+    public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+        return (List<E>) this.getCSVBean(reader, csvClass).parse();
     }
 
-    private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException
-    {
-        try
-        {
+    private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException {
+        try {
             CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             return csvToBeanBuilder.build();
-        }
-        catch (IllegalStateException e)
-        {
+        } catch (IllegalStateException e) {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.ILLEGAL_STATE, e.getMessage());
         }
     }
